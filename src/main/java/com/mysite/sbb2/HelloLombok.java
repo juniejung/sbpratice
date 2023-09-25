f7a5981bd2affdb4ffebd0ab03e0ea46b3a44643
juniejung <jooeunjung0429@gmail.com>
2023-09-25 21:08:12 +0900

    스프링부트 연습

---
+++ src/main/java/com/mysite/sbb2/HelloLombok.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb;
+
+
+import lombok.Getter;
+//import lombok.Setter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+//@Setter
+@RequiredArgsConstructor
+public class HelloLombok {
+    private final String hello;
+    private final int lombok;
+
+    public static void main(String[] ars){
+//        HelloLombok helloLombok = new HelloLombok();
+//        helloLombok.setHello("헬로");
+//        helloLombok.setLombok(5);
+
+        HelloLombok helloLombok = new HelloLombok("헬로",5);
+
+        System.out.println(helloLombok.getHello());
+        System.out.println(helloLombok.getLombok());
+    }
+}
